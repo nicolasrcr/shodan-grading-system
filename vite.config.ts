bee3c628be559531597de21b7de0f4618229e4a1
nicolasrcr <45941286+nicolasrcr@users.noreply.github.com>
2026-02-10 12:52:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/shodan-grading-system/",
   server: {
     host: "::",
     port: 8080,
