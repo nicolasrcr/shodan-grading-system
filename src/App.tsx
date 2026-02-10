11465868e12a94a4363aef7095a769743202e265
nicolasrcr <45941286+nicolasrcr@users.noreply.github.com>
2026-02-10 12:56:13 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -98,7 +98,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <AuthProvider>
           <AppRoutes />
         </AuthProvider>
